e663e08e614f89fba491fcd0b1dfe365dec3d6b3
Nicolas Tarbouriech <tarboun@hotmail.fr>
2023-06-15 17:23:51 +0200

    queue is refreshing dynamically

--- src/interface/user.interface.tsx
+++ src/interface/user.interface.tsx
@@ -8,4 +8,5 @@ export interface IUser extends Document {
   queue: IAction[];
   createdAt?: Date;
   updatedAt?: Date;
+  lastActionDeletedAt?: Date;
 }
--- src/pages/action.page.tsx
+++ src/pages/action.page.tsx
@@ -5,28 +5,27 @@ import axios from "axios";
 import { ActionTypeList } from "../component/type.list";
 import { io, Socket } from "socket.io-client";
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 
 export default function ActionPage() {
   const [credits, setCredits] = React.useState([0, 0, 0]);
-  const [type, setType] = React.useState('');
+  const [type, setType] = React.useState(null);
   const [actions, setActions] = useState([]);
 
-  React.useEffect(() => {
-    axios.get("/user/64888f96313b0d1f7ff2015b")
-      .then((res) => res.data)
-      .then((user) => {
-        setCredits([user.credits.A, user.credits.B, user.credits.C])
-        return user;
+  useEffect(() => {
+    axios.get("/user/648b2a6f093024a2aad39925")
+      .then((res) => {
+        setCredits([res.data.credits.A, res.data.credits.B, res.data.credits.C])
+        return res.data;
       })
       .then((user) => {
-          axios.get("/action/" + user._id)
-            .then((response) => {
-              setActions(response.data);
+        axios.get("/action/" + user._id)
+          .then((response) => {
+            setActions(response.data);
+          })
       })
-    })
   }, []);
 
   const handleSelectChange = (event: any) => {
@@ -37,24 +36,34 @@ export default function ActionPage() {
     const data = {
       type: type
     };
-    axios.post("/user/64888f96313b0d1f7ff2015b/actions", data)
-      .then(response => {
-        setActions(response.data)
+    axios.post("/user/648b2a6f093024a2aad39925/actions", data)
+      .then(() => {
+        axios.get("/action/648b2a6f093024a2aad39925")
+          .then(async (response) => {
+            setActions(response.data);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       })
       .catch(error => {
         console.error(error);
       });
   }
 
-  const [socket, setSocket] = useState<Socket| null>( null);
+  const [socket, setSocket] = useState<Socket | null>(null);
 
   useEffect(() => {
     const newSocket = io();
 
     newSocket.on("actionDeleted", () => {
-      axios.get("/action/64888f96313b0d1f7ff2015b")
+      axios.get("/action/648b2a6f093024a2aad39925")
         .then(async (response) => {
           setActions(response.data);
+          axios.get("/user/648b2a6f093024a2aad39925")
+            .then((res) => {
+              setCredits([res.data.credits.A, res.data.credits.B, res.data.credits.C])
+            })
         })
         .catch((error) => {
           console.error(error);
@@ -81,26 +90,26 @@ export default function ActionPage() {
           <div style={ {margin: '5px'} }>B : { credits[1] }</div>
           <div style={ {margin: '5px'} }>C : { credits[2] }</div>
         </div>
-        {actions.length > 0 ? (
-          <ActionTypeList actions={actions} />
+        { actions.length > 0 ? (
+          <ActionTypeList actions={ actions }/>
         ) : (
           <p>There is no action in the queue</p>
-        )}
-        <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+        ) }
+        <FormControl sx={ {m: 1, width: 300, mt: 3} }>
           <InputLabel id="demo-simple-select-label">Action</InputLabel>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            value={type}
+            value={ type }
             label="Type"
-            onChange={handleSelectChange}
+            onChange={ handleSelectChange }
           >
-            <MenuItem value={'A'}>A</MenuItem>
-            <MenuItem value={'B'}>B</MenuItem>
-            <MenuItem value={'C'}>C</MenuItem>
+            <MenuItem value={ 'A' }>A</MenuItem>
+            <MenuItem value={ 'B' }>B</MenuItem>
+            <MenuItem value={ 'C' }>C</MenuItem>
           </Select>
         </FormControl>
-        <Button sx={ {marginTop: '20px'} } variant={ "contained" } onClick={handleAddAction}>
+        <Button sx={ {marginTop: '20px'} } variant={ "contained" } onClick={ handleAddAction }>
           Add an action to the queue
         </Button>
       </Grid>
